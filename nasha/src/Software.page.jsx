3ea7ae5775edddee129bdf9ecebc87214213be89
Amin Gharibshah <amingharibshah@gmail.com>
2021-09-06 22:41:20 +0430

    finish snapshot part of software page

--- nasha/src/Software.page.jsx
+++ nasha/src/Software.page.jsx
@@ -1,6 +1,7 @@
 import SoftwareDetails from "./Components/SoftwareDetails/SoftwareDetails.component";
 import SoftwareDescription from "./Components/SoftwareDescription/SoftwareDescription.component";
 import Header from "./Components/Header/Header.component";
+import Snapshot from "./Components/Snapshot/Snapshot.component";
 
 import "./software.css";
 
@@ -10,6 +11,7 @@ const Software = () => {
       <Header type="3" className="type-3" nav="true" />
       <SoftwareDetails softwareName="cisco packet tracer" like={15} view={32} />
       <SoftwareDescription description="Packet Tracer is a cross-platform visual simulation tool designed by Cisco Systems that allows users to create network topologies and imitate modern computer networks. The software allows users to simulate the configuration of Cisco routers and switches using a simulated command line interface." />
+      <Snapshot />
     </div>
   );
 };
