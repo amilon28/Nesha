5503445166d1ca8e61680d9d2562bc72737e96cd
Amin Gharibshah <amingharibshah@gmail.com>
2021-09-13 21:02:07 +0430

    fix software page error

--- nasha/src/Components/MiddleBar/MiddleBar.component.jsx
+++ nasha/src/Components/MiddleBar/MiddleBar.component.jsx
@@ -5,18 +5,17 @@ const MiddleBar = (props) => {
 
   return (
     <div className="middle">
-      {!props.softwares &&
-        props.softwares.map((software) => {
-          return (
-            <MiddleCard
-              src={software.icon_picture}
-              likes={software.likes}
-              views={software.views}
-              date={software.date_submitted}
-              alt="program icon"
-            />
-          );
-        })}
+      {props.softwares?.map((software) => {
+        return (
+          <MiddleCard
+            src={software?.icon_picture}
+            likes={software?.likes}
+            views={software?.views}
+            date={software?.date_submitted}
+            alt="program icon"
+          />
+        );
+      })}
     </div>
   );
 };
